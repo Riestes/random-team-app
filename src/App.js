e1245a65decb82d757f55071f795ae6529d53f62
Riestes <jkf.koivula@gmail.com>
2021-03-04 21:04:55 +0200

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import ListPerson from './components/ListPerson';
 import axios from 'axios';
-import ListGroup from 'react-bootstrap/ListGroup'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
 
-import './App.css';
+import './App.scss';
 
 function App() {
 
   // How many users to fetch from API
   const userCount = 20;
-  
+
   const [persons, setPersons] = useState([]);
-  
   const [groupA, setGroupA] = useState([]);
   const [groupB, setGroupB] = useState([]);
+  const [activeIndexes, setActiveIndexes] = useState([]);
 
   // Get all all data from data service
   useEffect(() => {
@@ -27,80 +31,111 @@ function App() {
       });
   }, []);
 
-  const handleClick = (event) => {
+  // Change active state of list item when clicked
+  const changeActive = (event, index) => {
     event.preventDefault();
+    setActiveIndexes(activeIndexes.includes(index) ? activeIndexes.filter(i => i !== index) : activeIndexes.concat(index));
     event.target.classList.contains('active') ? event.target.classList.remove('active') : event.target.classList.add('active');
   }
 
-  console.log(persons);
-  const handleClickToGroupA = (event) => {
+  // Handle Click to Group A Or Group B
+  const handleClickToGroup = (event, activeIndexes) => {
     event.preventDefault();
-    for (let active of document.getElementsByClassName("active")) {
-      let firstName = active.innerText.split(' ')[0];
-      let lastName = active.innerText.split(' ')[1];
-      let foundPerson = persons.find(person => person.name.first.includes(firstName) && person.name.last.includes(lastName));
-      setPersons(persons.filter(person => person !== foundPerson));
-      
-      // console.log(foundPerson);
-      // setGroupA([...groupA, foundPerson]);
-      active.classList.remove('active');
+    // Find similar indexes
+    let foundPersons = [];
+    activeIndexes.map(i => {
+      let found = persons[i];
+      foundPersons.push(found);
+    });
+    setActiveIndexes(activeIndexes = []);
+    // Filter persons list
+    setPersons(persons.filter(person => !foundPersons.includes(person)));
+
+    // Remove active classes
+    let actives = document.getElementsByClassName('active');
+    while (actives.length > 0) {
+      actives[0].classList.remove('active');
     }
+    event.target.id === 'a-btn' ? setGroupA(groupA.concat(foundPersons)) : setGroupB(groupB.concat(foundPersons));
   }
 
-  const handleClickToGroupB = (event) => {
+  const handleRemoveClick = (event, index) => {
     event.preventDefault();
+    let selectedPerson = [];
+    if (event.target.id === 'a-group') {
+      selectedPerson = groupA[index];
+      setGroupA(groupA.filter(person => person !== selectedPerson));
+      
+    } else {
+      selectedPerson = groupB[index];
+      setGroupB(groupB.filter(person => person !== selectedPerson));
+    }
+    setPersons(persons.concat(selectedPerson));
   }
-
-  //console.log(groupA);
 
   return (
-    <div className="App">
-      <div>
-        <button onClick={handleClickToGroupA}>Group A</button>
-        <button onClick={handleClickToGroupB}>Group B</button>
-      </div>
-      <div>
-        <h1>BIGMAN</h1>
-        <ListGroup>
-          {persons.map((person, index) => {
-            return (
-              <ListPerson
-                index={index}
-                first={person.name.first}
-                last={person.name.last}
-                handleClick={handleClick}
-              />
-            );
-          })}
-        </ListGroup>
-      </div>
-      <div>
-        <h1>GroupA</h1>
-        <ListGroup>
-          {groupA.map((person, index) => {
-            return (
-              <ListPerson
-                index={index}
-                first={person.first}
-                last={person.last}
-                handleClick={handleClick}
-              />
-            );
-          })}
-        </ListGroup>
-     </div>
-      
-      {/* <ListGroup>
-        {groupB.map((person, index) => {
-          <ListPerson
-            index={index}
-            first={person.name.first}
-            last={person.name.last}
-            handleClick={handleClick}
-          />
-        })}
-      </ListGroup> */}
-   </div>
+    <Container className="App">
+      <h1>Teams app</h1>
+      <Row>
+        <Col xs={12} lg={4}>
+          <div className="inner-col">
+            <h4>Persons</h4>
+            <ListGroup className="list-group">
+              {persons.map((person, index) => {
+                return (
+                  <ListPerson
+                    index={index}
+                    first={person.name.first}
+                    last={person.name.last}
+                    changeActive={changeActive}
+                  />
+                );
+              })}
+            </ListGroup>
+          </div>
+          <div className="btn-group">
+            <Button variant="outline-success" id="a-btn" onClick={(event) => handleClickToGroup(event, activeIndexes)}>Move to Group A</Button>
+            <Button variant="outline-success" id="b-btn" onClick={(event) => handleClickToGroup(event, activeIndexes)}>Move to Group B</Button>
+          </div>
+        </Col>
+        <Col xs={12} sm={6} lg={4}>
+          <div className="inner-col">
+            <h4>Group A</h4>
+            <ListGroup className="list-group">
+              {groupA.map((person, index) => {
+                return (
+                  <ListPerson
+                    index={index}
+                    first={person.name.first}
+                    last={person.name.last}
+                    handleRemoveClick={handleRemoveClick}
+                    buttonGroup={'a-group'}
+                  />
+                );
+              })}
+            </ListGroup>
+          </div>
+        </Col>
+        <Col xs={12} sm={6} lg={4}>
+          <div className="inner-col">
+            <h4>Group B</h4>
+            <ListGroup className="list-group">
+              {groupB.map((person, index) => {
+                return (
+                  <ListPerson
+                    index={index}
+                    first={person.name.first}
+                    last={person.name.last}
+                    handleRemoveClick={handleRemoveClick}
+                    buttonGroup={'b-group'}
+                  />
+                );
+              })}
+            </ListGroup>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
--- src/components/ListPerson.js
+++ src/components/ListPerson.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import './ListPerson.scss';
 
-function ListPerson({ index, first, last, handleClick}) {
+function ListPerson({ index, first, last, changeActive, handleRemoveClick, buttonGroup }) {
+
+  const handleClicking = (event, index) => {
+    changeActive(event, index);
+  }
 
   return (
-    <ListGroup.Item onClick={(event) => handleClick(event)} key={index}>{`${first} ${last}`}</ListGroup.Item>
+    <>
+      {
+        changeActive
+          ? <ListGroup.Item action key={index} onClick={(event) => handleClicking(event, index)}><label>{`${first} ${last}`}</label></ListGroup.Item>
+          : <ListGroup.Item key={index}><label>{`${first} ${last}`}</label>
+            <Button id={buttonGroup} variant="outline-danger" size="sm" onClick={(event) => handleRemoveClick(event, index)}>Remove</Button>
+            </ListGroup.Item>
+      }
+    </>
+     
   );
 }
 
